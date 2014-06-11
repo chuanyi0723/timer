f35e03ec4dbbfa6d76413a5bbd3ab0622c591652
Chuan-Yi Wu <chuanyi0723@hotmail.com>
2014-06-12 00:09:36 +0800

    remove Timer from Panel

--- src/TimerFrame.java
+++ src/TimerFrame.java
@@ -5,28 +5,30 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 
 @SuppressWarnings("serial")
-public class TimerFrame extends JFrame implements ActionListener{
-	TimerPanel p = new TimerPanel();
+public class TimerFrame extends JFrame implements ActionListener {
+	TimerPanel p;
+	Timer t;
 
 	TimerFrame() {
 		setTitle("Timer");
 		setSize(300, 200);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		p.setT(new Timer(100, this));
-		p.getT().start();
+		p = new TimerPanel();
+		t = new Timer(100, this);
+		t.start();
 		add(p);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() == p.getT()) {
+		if (e.getSource() == t) {
 			int x = p.getPosX() + 10;
 			p.setPosX(x);
 			repaint();
 			if (x == 260) {
-				p.getT().stop();
+				t.stop();
 				System.exit(0);
 			}
 		}
--- src/TimerPanel.java
+++ src/TimerPanel.java
@@ -2,20 +2,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class TimerPanel extends JPanel {
-	private Timer t;
 	private int posX;
-
-	public Timer getT() {
-		return t;
-	}
-
-	public void setT(Timer t) {
-		this.t = t;
-	}
 
 	public int getPosX() {
 		return posX;
